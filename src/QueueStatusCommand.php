fe6ee0dbf59650e72ac524d520892a917b810130
Toby Allen <toby@cookingisfun.ie>
2024-10-26 07:29:49 +0100

    fix status

--- src/QueueStatusCommand.php
+++ src/QueueStatusCommand.php
@@ -12,7 +12,7 @@ class QueueStatusCommand extends QueueCount
      *
      * @var string
      */
-    protected $signature = 'queue:count {--queue= : Queue to query}
+    protected $signature = 'queue:status {--queue= : Queue to query}
                                         {--live}
                                         {--pause=3 : Number seconds to pause before rechecking status}
                                         ';
